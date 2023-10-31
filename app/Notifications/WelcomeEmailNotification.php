48f99b227caa4e9ed0372757e7ac05819bb5b3e4
syauqitaqiyuddin <ahmadsyauqi150@gmail.com>
2023-10-31 16:11:15 +0700

    initial commit

---
+++ app/Notifications/WelcomeEmailNotification.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\User;
+
+class WelcomeEmailNotification extends Notification
+{
+    use Queueable;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('Akun anda sudah terdaftarkan')
+                    ->action('Get help.', url('/'))
+                    ->line('Terima Kasih Sudah Menggunakan Layanan Kami!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
